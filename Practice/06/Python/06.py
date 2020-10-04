5b456042e7b3493f7afc64259426b11aba77f31a
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-04 22:17:28 +0300

    Second commit

---
+++ Practice/06/Python/06.py
@@ -0,0 +1,4 @@
+print("Введите значения a, b и c")
+a = int(input()); b = int(input()); c = int(input());
+if a = 0: x1 = -(c/b); print ("Корень: " + x1)
+if b = 0: d = -4*a*c; x1 = sqrt(d)/(2*a); x2 = -sqrt(d)/(2*a); print ("Корни: " + x1 + "," + x2)
---
+++ Practice/15/C++/Program 15/Program 15/Program 15.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(0));
+    int q, k = 1, i, a, n = rand() % 100; bool aha = false;
+    while (aha == false)
+    {
+        for (i = 1; i < 6; i++)
+        {
+            cout << "Попытка №" << k << "\nВведите число n: "; cin >> a;
+            if (a < n) cout << "Загаданное число больше\n\n";
+            else if (a > n) cout << "Загаданное число меньше\n\n";
+            else if (a = n)
+            {
+                cout << "Поздравляю! Вы угадали";
+                aha = true;
+            }
+            k++;
+        }
+        if (aha == true) { cout << "Хотите начать сначала? (1 - ДА)"; cin >> q; }
+        else cout << "Вы проиграли. Было загадано: " << n << "\nХотите начать сначала? (1 - ДА): "; cin >> q;
+        if (q == 1) break;
+    }
+}
