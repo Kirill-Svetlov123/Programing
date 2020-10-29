a65e9b3bdeaa85202c5b2bb7a9ada6055fdf6523
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-29 22:11:23 +0300

    Add 21/Python

---
+++ Practice/21/Python/21.py
@@ -0,0 +1,17 @@
+def bmi(weight: float, height: float) -> float:
+    return weight / height ** 2
+
+
+def print_bmi(bmi: float) -> float:
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi < 25:
+        print("Normal")
+    elif bmi < 30:
+        print("Overweight")
+    elif 30 > bmi:
+        print("Obesity")
+
+
+weight, height = map(float, input().split())
+print_bmi(bmi(weight, height * 0.01))
