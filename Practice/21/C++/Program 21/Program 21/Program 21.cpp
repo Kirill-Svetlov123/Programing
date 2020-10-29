7ed33dec1b1565b1fe2c3fc67e44955c46142ac7
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-29 23:21:27 +0300

    Add 21/C++

---
+++ Practice/21/C++/Program 21/Program 21/Program 21.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double BMI(double weight, double height)
+{
+    return weight / (height * height);
+}
+
+void printBMI(double BMI)
+{
+	if (BMI < 18.5)
+	{
+		std::cout << "Underweight" << std::endl;
+	}
+	else if (BMI < 25)
+	{
+		std::cout << "Normal" << std::endl;
+	}
+	else if (BMI < 30)
+	{
+		std::cout << "Overweight" << std::endl;
+	}
+	else
+	{
+		std::cout << "Obesity" << std::endl;
+	}
+}
+
+int main()
+{
+	double weight, height;
+    setlocale(LC_ALL, "Rus");
+	cin >> weight >> height;
+
+	printBMI(BMI(weight, height * 0.01));
+}
