ed517535a0b1b7db2722b25cc73f6b6a6d9c3a51
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-29 23:30:10 +0300

    16/Python

--- Practice/16/Python/16.py
+++ Practice/16/Python/16.py
@@ -1,20 +1,10 @@
 import re
 
-while True:
-
-	try:
-
-		n = int(input('Введите кол-во билетов: '))
-		
-		if (n < 1):
-			continue
-
-		test = re.match("^a[a-z][0-9]{2}55661$", test)
-
-		lst = [x for x in map(str, input().split(" ")) if test]
-
-		print(' '.join(test) if test else -1)
-		break
-
-	except Exception:
-		print('Ошибка!')
+n = int(input('Введите кол-во билетов N: '))
+s = str(input('Введите билеты: ').split(' '))
+pattern = r'\ba\w\d{2}55661\b'
+for i in range(n):
+	ticket = re.findall(pattern, s[i])
+	if True:
+		print(ticket)
+		continue
