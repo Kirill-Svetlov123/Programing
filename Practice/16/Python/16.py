91c22eb0e78ed3d66f776867d9008962c5f39bd2
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-30 22:38:59 +0300

    Add 16/Python

--- Practice/16/Python/16.py
+++ Practice/16/Python/16.py
@@ -3,8 +3,5 @@ import re
 n = int(input('Введите кол-во билетов N: '))
 s = str(input('Введите билеты: ').split(' '))
 pattern = r'\ba\w\d{2}55661\b'
-for i in range(n):
-	ticket = re.findall(pattern, s[i])
-	if True:
-		print(ticket)
-		continue
+ticket = re.findall(pattern, s[i])
+print(ticket)
