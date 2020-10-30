9dc82248522c2846818cd55f7d89fc3e119c01ca
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-10-30 22:36:28 +0300

    Add 18/C++

---
+++ Practice/18/C++/Program 18/Program 18/Program 18.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    string StopWord;
+    double AllSymbols = 0;
+
+    const vector <string> Words = { "hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen" };
+
+    vector <int> Symbols(257);
+
+    for (int i = 0; i < Words.size(); i++)
+    {
+        string s = Words[i];
+        AllSymbols += s.length();
+        for (int j = 0; j < s.length(); j++)
+        {
+            int word = int(s[j]);
+            Symbols[word] += 1;
+        }
+    }
+
+    cout << "Введите стоп-слово: ";
+    cin >> StopWord;
+
+    double probability = 1;
+
+    for (int i = 0; i < StopWord.length(); i++)
+    {
+        probability *= Symbols[int(StopWord[i])] / AllSymbols;
+    }
+
+    cout << probability;
+}
