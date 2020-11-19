90e4f3d23a0b0e84075b8a1ff308db026f63c065
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-11-19 13:07:00 +0300

    !

--- Practice/24/C++/Program 24/Program 24/Program 24.cpp
+++ Practice/24/C++/Program 24/Program 24/Program 24.cpp
@@ -14,39 +14,46 @@ typedef std::vector<Person> _persons;
 
 int main()
 {
+    bool flag = false;
     _persons persons;
     Person person;
     std::ifstream is("in.json");
     nlohmann::json j = nlohmann::json::parse(is);
     
-    for (int i = 0; j.size(); i++)
+    for (int i = 0; i < j.size(); i++)
     {
         int usid = j[i]["userId"];
         bool comp = j[i]["completed"];
         if (comp)
         {
-            for (int i = 0; i < persons.size(); i++)
+            for (int k = 0; k < persons.size(); i++)
             {
-                if (persons[i].userId == usid)
+                if (persons[k].userId == usid)
                 {
-                    persons[i].task_completed++;
+                    flag = true;
+                    persons[k].task_completed++;
+                    break;
                 }
             }
         }
-        person.task_completed = 1;
-        person.userId = usid;
-        persons.push_back(person);
+
+        if (!flag)
+        {
+            person.task_completed = 1;
+            person.userId = usid;
+            persons.push_back(person);
+        }
     }
 
-    nlohmann::json j = nlohmann::json::array();
+    nlohmann::json j1 = nlohmann::json::array();
     for (int i = 0; i < persons.size(); i++)
     {
         nlohmann::json uj;
         uj["userId"] = persons[i].userId;
         uj["task_completed"] = persons[i].task_completed;
-        j.push_back(uj);
+        j1.push_back(uj);
     }
 
     std::ofstream os("out.json");
-    os << std::setw(2) << j << std::endl;
+    os << std::setw(2) << j1 << std::endl;
 }
