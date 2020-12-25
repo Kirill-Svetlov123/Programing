3770e93bdaa76cd2aec02bca1d9244a22f070962
Kirill Svetlov <kirill_svetlov123@mail.ru>
2020-12-25 20:43:55 +0300

    fix Task 29

--- Practice/29/C++/Program 29/Program 29/Program 29.cpp
+++ Practice/29/C++/Program 29/Program 29/Program 29.cpp
@@ -1,52 +1,48 @@
 ﻿#include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
-
-string Info[60] = { "Petrov I", "5", "2", "4", "5", "3", "Ivanov K", "9", "5", "3", "4", "4", "Katrov P", "2", "4", "3", "5", "2", "Polkanova M", "7", "4", "5", "4", "5", "Alekseev N", "1", "3", "2", "3", "4", "Pavlova P", "8", "3", "4", "5", "3", "Tarakanov A", "5", "4", "5", "5", "3", "Fedorenko T", "6", "5", "5", "5", "2", "Pradedov K", "3", "3", "3", "5", "5", "Varkov E", "1", "4", "5", "3", "4"};
-
-string columns = "Name\t\t\tGroup\t\tMath\t\tPhys\t\tHist\t\tProg";
 
 struct Student {
     string name;
     int group;
-    int exams[4];
+    vector <int> exams;
 };
 
-vector <Student> list_formation(vector <Student> Stud) {
-    for (int i = 0; i < 10; i++) {
-        Stud = input_struct(i, Stud);
+int search_the_longest(vector <Student> Losers) {
+    int max = -1;
+    for (int i = 0; i < Losers.size(); i++) {
+        if (atoi(Losers[i].name.c_str()) > max) {
+            max = atoi(Losers[i].name.c_str());
+        }
     }
-    return Stud;
-}
-
-vector <Student> input_struct(int i, vector <Student> Stud) {
-    Stud[i].name = Info[6*i];
-    Stud[i].group = atoi(Info[6*i+1].c_str()); 
-    for (int j = 0; j < 4; j++) {
-        Stud[i].exams[j] = atoi(Info[6*i+2+j].c_str());
-    }
-    return Stud;
+    return max;
 }
 
 ostream& operator<<(ostream& os, Student& student) {
-    os << "\n\n" << Stud[i].name << "\t\t" << Stud[i].group << "\t\t";
-    for (int j = 0; j < 4; j++) {
-        os << Stud[i].exams[j] << "\t\t";
-    }
-    return os;
+    
 }
 
 int main()
 {
-    vector <Student> Stud;
-    Stud.resize(10);
+    vector <Student> Stud = {
+        { "Petrov I", 5, {2, 4, 5, 3} },
+        { "Ivanov K", 9, {5, 3, 4, 4} },
+        { "Katrov P", 2, {4, 3, 5, 2} },
+        { "Polkanova M", 7, {4, 5, 4, 5} },
+        { "Alekseev N", 1, {3, 2, 3, 4} },
+        { "Pavlova P", 8, {3, 4, 5, 3} },
+        { "Tarakanov A", 5, {4, 5, 5, 3} },
+        { "Fedorenko T", 6, {5, 5, 5, 2} },
+        { "Pradedov K", 3, {3, 3, 5, 5} },
+        { "Varkov E", 1, {4, 5, 3, 4} } };
 
-    Stud = list_formation(Stud);
-
-    cout << columns;
-    for (int i = 0; i < 10; i++) {
-        cout << Stud;
+    vector<Student> Losers;
+    for (int i = 0; i < Stud.size(); i++) {
+        if ((Stud[i].exams[0] == 2) || (Stud[i].exams[1] == 2) || (Stud[i].exams[2] == 2) || (Stud[i].exams[3] == 2)) {
+            Losers.push_back(Stud[i]);
+        }
     }
+    
+    int the_longest = search_the_longest(Losers);
 }
