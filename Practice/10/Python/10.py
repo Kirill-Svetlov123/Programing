5eba20edda7fb0656b1288c5d7e03da0ae6b9bc2
Kirill-Svetlov123 <70909585+Kirill-Svetlov123@users.noreply.github.com>
2021-01-22 10:09:21 +0300

    Update 10.py

--- Practice/10/Python/10.py
+++ Practice/10/Python/10.py
@@ -1,20 +1,13 @@
-while True:
-	try:
-		l1, r1 = map(int, input('Введите первый и последний элемент 1-го диапозона: ').split(' '))
-		l2, r2 = map(int, input('Введите первый и последний элемент 2-го диапозона: ').split(' '))
-		s = int(input('Введите искомую сумму: '))
+l1, r1 = map(int, input('Введите первый и последний элемент 1-го диапозона: ').split(' '))
+l2, r2 = map(int, input('Введите первый и последний элемент 2-го диапозона: ').split(' '))
+s = int(input('Введите искомую сумму: '))
 
-		if (l1 + r2 < s):
-			l1 = min(s-r2, r1)
-		else:
-			r2 = max(s-l1, l2)
+if (l1 + r2 < s):
+	l1 = min(s-r2, r1)
+else:
+	r2 = max(s-l1, l2)
 
-		if l1 + r2 == s:
-			print(l1,r2)
-		else:
-			print(-1)
-
-		break
-		
-	except Exception:
-		print('Ошибка!')
+if (l1 + r2 == s):
+	print(l1,r2)
+else:
+	print(-1)
